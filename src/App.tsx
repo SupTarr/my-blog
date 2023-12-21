804a386133eac9e6644d1d1b16d522cba076dfb5
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-21 20:56:29 +0700

    feat: apply router to all page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PageHome from "./pages/PageHome";
+import PageNewPost from "./pages/PageNewPost";
+import PagePost from "./pages/PagePost";
+import PageAbout from "./pages/PageAbout";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
     <main className="app min-h-screen">
       <Header />
+      <Routes>
+        <Route path="/" element={<PageHome />} />
+        <Route path="/post" element={<PageNewPost />} />
+        <Route path="/post/:id" element={<PagePost />} />
+        <Route path="/about" element={<PageAbout />} />
+        <Route path="*" Component={Page404} />
+      </Routes>
       <Footer />
     </main>
   );
