4547a83143cf594b1ce390e9141059ccacc11571
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-22 17:16:16 +0700

    fix: fix width of container

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -2,7 +2,7 @@ const PageAbout = () => {
   return (
     <main className="mx-auto flex w-full max-w-screen-lg flex-col flex-wrap content-center gap-5 p-2">
       <h1 className="mb-2 w-72 text-2xl font-semibold md:min-w-96">About</h1>
-      <p>
+      <p className="w-72 md:min-w-96">
         This blog app is a project in the{" "}
         <a
           className="link"
